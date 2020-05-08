1b54fd1f8be226237569d39da2afb195e83ed5fd
ialarmedalien <ialarmedalien@gmail.com>
2020-05-08 08:30:13 -0700

    adding cat_fasta_files test, check refs before adding assembly/binned contigs/etc

--- test/core_checkM_test.py
+++ test/core_checkM_test.py
@@ -7,6 +7,10 @@ import shutil
 import csv
 import logging
 import sys
+from uuid import uuid4
+from itertools import repeat
+from random import randint
+import tempfile
 
 from os import environ
 from configparser import ConfigParser
@@ -33,6 +37,7 @@ from kb_Msuite.Utils.Utils import LogMixin, TSVMixin
 from kb_Msuite.Utils.WorkspaceHelper import WorkspaceHelper
 
 from kb_Msuite.Utils.FileUtils import (
+    cat_fasta_files,
     clean_up_bin_ID,
     fasta_seq_len_at_least,
     get_fasta_files,
@@ -289,6 +294,11 @@ class CoreCheckMTest(unittest.TestCase, LogMixin, TSVMixin):
         }
 
         '''
+        if hasattr(self, assembly['attr']):
+            self.logger.info({
+                assembly['attr']: getattr(self, assembly['attr']),
+            })
+            return
 
         assembly_file_path = os.path.join(self.test_data_dir, "assemblies", assembly['path'])
         if not os.path.exists(assembly_file_path):
@@ -315,6 +325,12 @@ class CoreCheckMTest(unittest.TestCase, LogMixin, TSVMixin):
         }
 
         '''
+        if hasattr(self, assemblyset['attr']):
+            self.logger.info({
+                assemblyset['attr']: getattr(self, assemblyset['attr']),
+            })
+            return
+
         saved_assembly_set = self.setAPI.save_assembly_set_v1({
             'workspace_name': self.refdata_ws_info[1],
             'output_object_name': assemblyset['name'],
@@ -358,6 +374,11 @@ class CoreCheckMTest(unittest.TestCase, LogMixin, TSVMixin):
         return True
 
     def _prep_binned_contig(self, bc):
+        if hasattr(self, bc['attr']):
+            self.logger.info({
+                bc['attr']: getattr(self, bc['attr']),
+            })
+            return
 
         binned_contigs_path = os.path.join(self.test_data_dir, bc['path'])
         if not os.path.exists(binned_contigs_path) or not os.path.exists(
@@ -395,6 +416,11 @@ class CoreCheckMTest(unittest.TestCase, LogMixin, TSVMixin):
         return True
 
     def _prep_genome(self, genome):
+        if hasattr(self, genome['attr']):
+            self.logger.info({
+                genome['attr']: getattr(self, genome['attr']),
+            })
+            return
 
         genome_file_path = os.path.join(self.test_data_dir, genome['path'])
         if not os.path.exists(genome_file_path):
@@ -413,6 +439,12 @@ class CoreCheckMTest(unittest.TestCase, LogMixin, TSVMixin):
         })
 
     def _prep_genomeset(self, genomeset):
+        if hasattr(self, genomeset['attr']):
+            self.logger.info({
+                genomeset['attr']: getattr(self, genomeset['attr']),
+            })
+            return
+
         [OBJID_I, NAME_I, TYPE_I, SAVE_DATE_I, VERSION_I, SAVED_BY_I, WSID_I,
          WORKSPACE_I, CHSUM_I, SIZE_I, META_I] = list(range(11))  # object_info tuple
 
@@ -472,6 +504,9 @@ class CoreCheckMTest(unittest.TestCase, LogMixin, TSVMixin):
 
     def prep_report(self):
         ''' copy templates into the test data directory and create a KBaseReport ref '''
+
+        if hasattr(self, 'report_ref'):
+            return
 
         # copy over the templates
         test_tmpl_dir = os.path.join(self.test_data_dir, 'templates')
@@ -843,63 +878,58 @@ class CoreCheckMTest(unittest.TestCase, LogMixin, TSVMixin):
 
     def test_01_fileutils_set_fasta_file_extensions(self):
 
-        cmu = self.prep_checkMUtil()
-        run_config = cmu.run_config()
-        test_dir = os.path.join(run_config['base_dir'], 'test_dir')
-        os.makedirs(test_dir, exist_ok=True)
+        with tempfile.TemporaryDirectory() as test_dir:
 
-        extensions = ['.fasta', '.fas', '.fa', '.fsa', '.seq', '.fna', '.ffn', '.faa', '.frn']
-        invalid_ext = ['.jpg', '.fasta.tar.gz', '.fa.zip', '.fnaaaar']
+            extensions = ['.fasta', '.fas', '.fa', '.fsa', '.seq', '.fna', '.ffn', '.faa', '.frn']
+            invalid_ext = ['.jpg', '.fasta.tar.gz', '.fa.zip', '.fnaaaar']
 
-        dir_inventory = {}
-        n = 0
-        for ext in extensions + invalid_ext:
-            file_path = os.path.join(test_dir, 'file_00' + str(n) + ext)
-            Path(file_path).touch()
-            self.assertTrue(os.path.isfile(file_path))
-            dir_inventory['file_00' + str(n)] = file_path
-            n += 1
+            dir_inventory = {}
+            n = 0
+            for ext in extensions + invalid_ext:
+                file_path = os.path.join(test_dir, 'file_00' + str(n) + ext)
+                Path(file_path).touch()
+                self.assertTrue(os.path.isfile(file_path))
+                dir_inventory['file_00' + str(n)] = file_path
+                n += 1
 
-        fasta_files_by_clean_bin_ID = get_fasta_files(test_dir, 'fasta')
-        self.assertEqual({'file_000': dir_inventory['file_000']}, fasta_files_by_clean_bin_ID)
+            fasta_files_by_clean_bin_ID = get_fasta_files(test_dir, 'fasta')
+            self.assertEqual({'file_000': dir_inventory['file_000']}, fasta_files_by_clean_bin_ID)
 
-        # no files with the new extension (yet)
-        self.assertEqual({}, get_fasta_files(test_dir, '007'))
+            # no files with the new extension (yet)
+            self.assertEqual({}, get_fasta_files(test_dir, '007'))
 
-        set_fasta_file_extensions(test_dir, '007')
-        # expect 1 - 9 to have the extension .007
-        new_dir_inventory = {}
-        n = 0
-        for ext in extensions:
-            ext_file = os.path.join(test_dir, 'file_00' + str(n) + '.007')
-            self.assertTrue(os.path.isfile(ext_file))
-            new_dir_inventory['file_00' + str(n)] = ext_file
-            n += 1
+            set_fasta_file_extensions(test_dir, '007')
+            # expect 1 - 9 to have the extension .007
+            new_dir_inventory = {}
+            n = 0
+            for ext in extensions:
+                ext_file = os.path.join(test_dir, 'file_00' + str(n) + '.007')
+                self.assertTrue(os.path.isfile(ext_file))
+                new_dir_inventory['file_00' + str(n)] = ext_file
+                n += 1
 
-        for ext in invalid_ext:
-            pos = n - len(extensions)
-            self.assertTrue(os.path.isfile(
-                os.path.join(test_dir, 'file_00' + str(n) + invalid_ext[pos]))
-            )
-            n += 1
+            for ext in invalid_ext:
+                pos = n - len(extensions)
+                self.assertTrue(os.path.isfile(
+                    os.path.join(test_dir, 'file_00' + str(n) + invalid_ext[pos]))
+                )
+                n += 1
 
-        # ensure that we get the expected results from get_fasta_files
-        fasta_files_by_clean_bin_ID = get_fasta_files(test_dir, 'fasta')
-        self.assertEqual({}, get_fasta_files(test_dir, 'fasta'))
-        self.assertEqual(new_dir_inventory, get_fasta_files(test_dir, '007'))
+            # ensure that we get the expected results from get_fasta_files
+            fasta_files_by_clean_bin_ID = get_fasta_files(test_dir, 'fasta')
+            self.assertEqual({}, get_fasta_files(test_dir, 'fasta'))
+            self.assertEqual(new_dir_inventory, get_fasta_files(test_dir, '007'))
 
-        # check that set_fasta_file_extensions will happily overwrite an extant file
-        # TODO: it would be better if this complained, rather than automatically
-        # overwriting the file
-        new_file = os.path.join(test_dir, 'file_007.faa')
-        Path(os.path.join(test_dir, 'file_007.faa')).touch()
-        self.assertTrue(os.path.isfile(new_file))
-        self.assertTrue(os.path.isfile(os.path.join(test_dir, 'file_007.007')))
-        set_fasta_file_extensions(test_dir, '.007')
-        self.assertFalse(os.path.exists(new_file))
-        self.assertTrue(os.path.isfile(os.path.join(test_dir, 'file_007.007')))
-
-        self.clean_up_cmu(cmu)
+            # check that set_fasta_file_extensions will happily overwrite an extant file
+            # TODO: it would be better if this complained, rather than automatically
+            # overwriting the file
+            new_file = os.path.join(test_dir, 'file_007.faa')
+            Path(os.path.join(test_dir, 'file_007.faa')).touch()
+            self.assertTrue(os.path.isfile(new_file))
+            self.assertTrue(os.path.isfile(os.path.join(test_dir, 'file_007.007')))
+            set_fasta_file_extensions(test_dir, '.007')
+            self.assertFalse(os.path.exists(new_file))
+            self.assertTrue(os.path.isfile(os.path.join(test_dir, 'file_007.007')))
 
     def test_01_fileutils_read_bin_stats_file(self):
 
@@ -908,22 +938,50 @@ class CoreCheckMTest(unittest.TestCase, LogMixin, TSVMixin):
 
         # this could do with more testing...
 
-    def notest_01_fileutils_cat_fasta_files(self):
-        # folder, extension, output_fasta_file):
-        '''
-        Given a folder of fasta files with the specified extension, cat them together
-        using 'cat' into the target new_fasta_file
-        '''
-        # files = glob.glob(os.path.join(folder, '*.' + extension))
-        # cat_cmd = ['cat'] + files
-        # fasta_fh = open(output_fasta_file, 'w')
-        # p = subprocess.Popen(cat_cmd, cwd=self.scratch, stdout=fasta_fh, shell=False)
-        # exitCode = p.wait()
-        # fasta_fh.close()
+    def check_cat_files(self, target_dir, ext, output_file, expected_lines):
 
-        # if exitCode != 0:
-        #     raise ValueError('Error running command: ' + ' '.join(cat_cmd) + '\n' +
-        #                      'Exit Code: ' + str(exitCode))
+        self.assertFalse(os.path.exists(output_file))
+        cat_fasta_files(target_dir, ext, output_file)
+        self.assertTrue(os.path.isfile(output_file))
+
+        with open(output_file, 'r') as output_fh:
+            lines = output_fh.read()
+
+        # split on 'FILE: file_' to create an array of the different file contents
+        sep = 'FILE: file_'
+        expected_lines = [sep + _ for _ in lines.split(sep) if _]
+        self.assertEqual(set(lines), set(expected_lines))
+
+    def make_file_contents(self, filename):
+        # generate a random number of strings with which to populate the file
+        random_text = "".join([str(uuid4()) + '\n' for _ in repeat(None, randint(1, 6))])
+        return 'FILE: ' + filename + '\n' + random_text
+
+    def test_01_fileutils_cat_fasta_files(self):
+
+        with tempfile.TemporaryDirectory() as tempdir:
+            # set up a test directory with several files with different extensions
+            target_dir = os.path.join(tempdir, 'test_files')
+            results_dir = os.path.join(tempdir, 'results')
+            os.makedirs(target_dir)
+            os.makedirs(results_dir)
+
+            ext_list = ['fasta', 'fa.zip', 'fa', 'fasta.tar.gz', 'fasta', 'fa', 'fa', 'far']
+            filenames = ['file_' + str(_) + '.' + ext_list[_] for _ in range(len(ext_list))]
+            files = {f: self.make_file_contents(f) for f in filenames}
+
+            for name, contents in files.items():
+                file_path = os.path.join(target_dir, name)
+                with open(file_path, 'w') as fh:
+                    fh.write(contents)
+
+                self.assertTrue(os.path.isfile(file_path))
+                self.assertTrue(os.path.getsize(file_path) > 0)
+
+            for ext in ['fasta', 'fa', 'tar']:
+                output_file = os.path.join(results_dir, ext + '_concat_file.txt')
+                expected_lines = [files[f] for f in files.keys() if f.endswith(ext)]
+                self.check_cat_files(target_dir, ext, output_file, expected_lines)
 
     def test_02_data_staging(self):
 
