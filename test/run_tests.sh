99a0a0bc8e4acc5a94879a26a19a404b29029dd8
ialarmedalien <ialarmedalien@gmail.com>
2020-05-12 16:03:16 -0700

    back to using nose

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -10,5 +10,5 @@ python -m compileall lib/ test/
 cd $script_dir/../test
 rm -rf tmp
 mkdir -p tmp
-PYTHONPATH=$APPDIR/lib/:$APPDIR/test/:$PYTHONPATH coverage run -m unittest -v kb_Msuite_testSuite.py
-#python -m nose --with-coverage --cover-package=kb_Msuite --cover-html --cover-html-dir=/kb/module/work/test_coverage --nocapture  --nologcapture .
+#PYTHONPATH=$APPDIR/lib/:$APPDIR/test/:$PYTHONPATH coverage run -m unittest -v kb_Msuite_testSuite.py
+python -m nose --with-coverage --cover-package=kb_Msuite --cover-html --cover-html-dir=/kb/module/work/test_coverage --nocapture  --nologcapture .
