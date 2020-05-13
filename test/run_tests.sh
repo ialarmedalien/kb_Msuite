333fb0a264562f6183a349607b36a49d0a36609a
ialarmedalien <ialarmedalien@gmail.com>
2020-05-13 06:49:57 -0700

    doh! stop test runner from removing tmp contents

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -12,7 +12,7 @@ export PYTHONPATH=$APPDIR/lib:$APPDIR/test:$PYTHONPATH
 cd $script_dir/..
 python -m compileall lib/ test/
 cd $script_dir/../test
-rm -rf tmp
-mkdir -p tmp
+# rm -rf tmp
+# mkdir -p tmp
 PYTHONPATH=$APPDIR/lib/:$APPDIR/test/:$PYTHONPATH coverage run -m unittest -v kb_Msuite_testSuite.py
 #python -m nose --with-coverage --cover-package=kb_Msuite --cover-html --cover-html-dir=/kb/module/work/test_coverage --nocapture  --nologcapture .
